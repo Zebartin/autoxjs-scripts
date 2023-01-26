a3426f223c84d4b50b252cd7a46b6f40fc510d35
Zebartin <1500012724@pku.edu.cn>
2023-01-26 13:15:39 +0800

    fix&feat: NIKKEutils repeat combat

--- NIKKE/NIKKEutils.js
+++ NIKKE/NIKKEutils.js
@@ -1,11 +1,24 @@
-module.exports = {
-  启动NIKKE: 启动NIKKE,
-  退出NIKKE: 退出NIKKE,
-  返回首页: 返回首页
-};
 var { ocrUntilFound, clickRect } = require('./utils.js');
+var { width, height } = device;
+if (typeof module === 'undefined') {
+  var {
+    unlockIfNeed,
+    requestScreenCaptureAuto
+  } = require('./utils.js');
+  auto.waitFor();
+  unlockIfNeed();
+  requestScreenCaptureAuto();
+  刷刷刷();
+  exit();
+}
+else {
+  module.exports = {
+    启动NIKKE: 启动NIKKE,
+    退出NIKKE: 退出NIKKE,
+    返回首页: 返回首页
+  };
+}
 function 启动NIKKE() {
-  const { width, height } = device;
   device.setMusicVolume(0);
   if (ocrUntilFound(res => res.text.match(/(大厅|基地|物品|方舟)/), 5, 400) != null)
     return;
@@ -36,12 +49,12 @@ function 启动NIKKE() {
       click(width / 2, height * 0.8);
       sleep(40 * 1000);
     }
-    else if (res.text.includes('正在下载')){
+    else if (res.text.includes('正在下载')) {
       sleep(10000);
       return false;
     }
     else if (res.text.match(/(確認|确认)/) != null) {
-      clickRect(res.find(e=>e.text.match(/(確認|确认)$/) != null));
+      clickRect(res.find(e => e.text.match(/(確認|确认)$/) != null));
       return false;
     }
     else if (res.text.includes('登出'))
@@ -55,8 +68,9 @@ function 启动NIKKE() {
   sleep(1000);
   click(width / 2, height * 0.9);
   // 检查是否有daily login
-  if (ocrUntilFound(res => res.find(e => 
-    e.text.match(/D\s*A\s*I\s*L\s*Y/i) != null && e.bounds.left >= width / 2
+  if (ocrUntilFound(res => res.find(e =>
+    e.text.match(/D\s*A\s*I\s*L\s*Y/i) != null &&
+    e.bounds != null && e.bounds.left >= width / 2
   ), 10, 1000) != null)
     back();
 }
@@ -88,11 +102,33 @@ function 返回首页() {
   }
   homeImage.recycle();
   sleep(1000);
-  while(true){
+  while (true) {
     click(result.x, result.y);
     sleep(2000);
     if (ocrUntilFound(res => res.text.match(/(大厅|基地|物品|方舟)/), 5, 400) != null)
       break;
   }
   log('返回首页');
+}
+
+function 刷刷刷() {
+  clickRect(ocrUntilFound(res => res.find(
+    e => e.text.endsWith('战斗')
+  ), 20, 3000));
+  log('进入战斗');
+  sleep(2000);
+  if (ocrUntilFound(res => res.text.includes('AUT'), 10, 1000) != null) {
+    while (true) {
+      sleep(20 * 1000);
+      ocrUntilFound(res => res.text.includes('REWARD'), 30, 3000);
+      var target = ocrUntilFound(res => res.find(
+        e => e.text.endsWith('重新开始')
+      ), 20, 1000);
+      if (colors.blue(captureScreen().pixel(target.bounds.left, target.bounds.top)) < 230)
+        break;
+      clickRect(target);
+    }
+    log('门票用完了');
+    click(width / 2, height / 2);
+  }
 }

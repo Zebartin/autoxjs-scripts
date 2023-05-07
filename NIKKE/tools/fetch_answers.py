2dea7a06b568770f36a0d699e8e48376e3b2d8b2
Zebartin <1500012724@pku.edu.cn>
2023-05-07 13:51:11 +0800

    fix: 适配gamekee咨询的排版变动

--- NIKKE/tools/fetch_answers.py
+++ NIKKE/tools/fetch_answers.py
@@ -7,45 +7,59 @@ import requests
 from collections import defaultdict
 from bs4 import BeautifulSoup as bs
 
+session = requests.Session()
+
 # ord('⋯') = 8943
 # ord('…') = 8230
 punctuation_pattern = re.compile(r"[，⋯…？?、!！「」～☆【】。.—{}'\"“”♪\s]")
 
-
-def getFromGamekee():
-    r = requests.get('https://nikke.gamekee.com/v1/content/detail/575965', headers={
-        'game-alias': 'nikke'
-    })
-    soup = bs(r.json()['data']['content'], 'html.parser')
+def get_from_gamekee():
+    detail_url = 'https://nikke.gamekee.com/v1/content/detail/{detail_id}'
+    detail_ids = ['575965']
+    page = 0
     ret = {}
-    person = None
-    answers = []
-    for div in soup.contents[-1].find_all('div'):
-        if div.find('div') != None:
-            continue
-        line = div.text.strip()
-        if len(line) == 0:
-            continue
-        if line.startswith('50') or line.startswith('- question'):
-            continue
-        if line.startswith('100'):
-            if line.find('AccountData') != -1:
-                continue
-            a = punctuation_pattern.sub('', line[5:])
-            if len(a) == 0:
-                continue
-            answers.append(a)
-        else:
-            if person is not None and len(answers) > 0:
-                ret[person] = answers
-                answers = []
-            person = line[:-1]
-    if len(answers) > 0:
-        ret[person] = answers
+    while page < len(detail_ids):
+        print(f'https://nikke.gamekee.com/{detail_ids[page]}.html')
+        r = session.get(detail_url.format(detail_id=detail_ids[page]), headers={
+            'game-alias': 'nikke'
+        })
+        soup = bs(r.json()['data']['content'], 'html.parser')
+        result = {}
+        person = None
+        answers = []
+        # 需要获取其他分页
+        if len(detail_ids) == 1:
+            detail_ids.extend(re.findall('nikke.gamekee.com/(\d+).html', str(soup)))
+        for content in soup.contents:
+            for div in content.find_all('div'):
+                if div.find('div') != None:
+                    continue
+                line = div.text.strip()
+                if len(line) == 0:
+                    continue
+                if line.startswith('50') or 'question' in line:
+                    continue
+                if line.startswith('100'):
+                    if line.find('AccountData') != -1:
+                        continue
+                    a = punctuation_pattern.sub('', line[5:])
+                    if len(a) == 0:
+                        continue
+                    answers.append(a)
+                else:
+                    if person is not None and '角色名' not in person and len(answers) > 0:
+                        result[person] = answers
+                    person = line[:-1]
+                    answers = []
+        if len(answers) > 0:
+            result[person] = answers
+        print(', '.join(list(result.keys())))
+        ret.update(result)
+        page += 1
     return ret
 
 
-def getFromGoogleSheet(apiKey):
+def get_from_google_sheet(apiKey):
     ret = defaultdict(list)
     # 定义要访问的google表格的ID和范围
     spreadsheetId = '1K_oGZWL4uvuqYM9JL2l5Kb-L0IXIuyrDI347b6rurho'
@@ -53,7 +67,7 @@ def getFromGoogleSheet(apiKey):
     url = f'https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values:batchGet'
 
     # 发送GET请求，获取第一列和第三列的内容
-    response = requests.get(url, params={
+    response = session.get(url, params={
         'key': apiKey,
         'ranges': ['角色排序查詢!A2:A', '角色排序查詢!C2:C']
     })
@@ -87,8 +101,8 @@ def getFromGoogleSheet(apiKey):
 
 
 if __name__ == '__main__':
-    zh_cn_data = getFromGamekee()
-    zh_tw_data = getFromGoogleSheet(sys.argv[1])
+    zh_cn_data = get_from_gamekee()
+    zh_tw_data = get_from_google_sheet(sys.argv[1])
     # 在gamekee数据基础上，补充繁中服的数据
     for k in zh_tw_data:
         if k in zh_cn_data:
